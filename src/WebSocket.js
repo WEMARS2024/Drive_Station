a6dc0d75c7e0d66a2faae2b94edde886ac7bfb71
bryanh002 <heddlebryan0@gmail.com>
2024-08-10 08:06:32 -0600

    fun times

--- src/WebSocket.js
+++ src/WebSocket.js
@@ -1,18 +1,28 @@
 import React, { useContext } from 'react';
 import WebSocketContext from './WebSocketContext';
 
-
 const WebSocketComponent = () => {
-  const data = useContext(WebSocketContext);
+  const { data, initialData } = useContext(WebSocketContext);
 
   return (
-    <div id="Imu Data" className="relative right-[38px] top-[115px] w-[220px] h-[235px] overflow-hidden p-10 rounded-lg border-solid border-4 border-black bg-slate-200 shadow-lg">
-      <h1 className="text-xl pb-2 font-serif">IMU Data</h1>
-      <p className="text-lg pb-2">Pitch [x]: {data.pitch}</p>
-      <p className="text-lg pb-2">Roll [y]: {data.roll}</p>
-      <p className="text-lg pb-2">Yaw [z]: {data.yaw}</p>
+    <div id="Imu Data" className="relative flex right-[38px] top-[115px] w-[450px] h-[235px] overflow-hidden p-10 rounded-lg border-solid border-4 border-black bg-slate-200 shadow-lg">
+      
+      <div>
+        <h1 className="text-xl pb-2 font-serif">IMU Data</h1>
+        <p className="text-lg pb-2">Pitch [x]: {data.pitch}</p>
+        <p className="text-lg pb-2">Roll [y]: {data.roll}</p>
+        <p className="text-lg pb-2">Yaw [z]: {data.yaw}</p>
+      </div>
+
+      <div className="pl-28">
+        <h1 className="text-xl pb-2 font-serif">Header Data</h1>
+        <p className="text-lg pb-2">Pitch [x]: {initialData?.pitch}</p>
+        <p className="text-lg pb-2">Roll [y]: {initialData?.roll}</p>
+        <p className="text-lg pb-2">Yaw [z]: {initialData?.yaw}</p>
+      </div>
     </div>
   );
 };
 
 export default WebSocketComponent;
+
--- src/WebSocketContext.js
+++ src/WebSocketContext.js
@@ -12,8 +12,10 @@ export const WebSocketProvider = ({ children }) => {
     speed: null,
   });
 
+  const [initialData, setInitialData] = useState(null);
+
   useEffect(() => {
-    const ws = new WebSocket('ws://192.168.137.235:5000');
+    const ws = new WebSocket('ws://192.168.1.4:5000');
 
     ws.onopen = () => {
       console.log('Connected to WebSocket server');
@@ -24,14 +26,20 @@ export const WebSocketProvider = ({ children }) => {
         const message = event.data;
         const dataArray = message.split(',');
 
-        setData({
+        const newData = {
           pitch: parseFloat(dataArray[0]),
           roll: parseFloat(dataArray[1]),
           yaw: parseFloat(dataArray[2]),
           longitude: parseFloat(dataArray[3]),
           latitude: parseFloat(dataArray[4]),
           speed: parseFloat(dataArray[5]),
-        });
+        };
+
+        setData(newData);
+
+        if (!initialData) {
+          setInitialData(newData);
+        }
       } catch (error) {
         console.error('Error parsing WebSocket message', error);
       }
@@ -48,10 +56,10 @@ export const WebSocketProvider = ({ children }) => {
     return () => {
       ws.close();
     };
-  }, []);
+  }, [initialData]);
 
   return (
-    <WebSocketContext.Provider value={data}>
+    <WebSocketContext.Provider value={{ data, initialData }}>
       {children}
     </WebSocketContext.Provider>
   );
